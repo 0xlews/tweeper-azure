9a79c3bec1b193cb1fc0be1c0512182180a3178b
Lewis Wigmore <lewis.wigmore@protonmail.com>
2024-01-21 16:26:41 +0000

    Update error handling

--- Tweep/__init__.py
+++ Tweep/__init__.py
@@ -17,7 +17,7 @@ def get_key_vault_secret(secret_name):
     retrieved_secret = client.get_secret(secret_name)
     return retrieved_secret.value
 
-# Initialize Blob Service Client
+# Initialise Blob Service Client
 storage_connection_string = get_key_vault_secret('your-storage-connection-string')
 blob_service_client = BlobServiceClient.from_connection_string(storage_connection_string)
 
@@ -34,17 +34,18 @@ twitter_client = tweepy.Client(
     access_token=twitter_access_token,
     access_token_secret=twitter_access_token_secret
 )
+logging.info('Initialised Twitter client')
 
 # Reddit API initialisation
-reddit_client_id = get_key_vault_secret('reddit-client-id')
-reddit_client_secret = get_key_vault_secret('reddit-client-secret')
+reddit_client_id = get_key_vault_secret('your-reddit-client-id')
+reddit_client_secret = get_key_vault_secret('your-reddit-client-secret')
 logging.info('Retrieved reddit client secret from key vault')
 reddit_user_agent = 'your-reddit-user-agent'
 
 reddit = praw.Reddit(client_id=reddit_client_id,
                      client_secret=reddit_client_secret,
                      user_agent=reddit_user_agent)
-logging.info('Initialized Reddit client')
+logging.info('Initialised Reddit client')
 
 # Functions to read and write blobs
 def read_blob(container_name, blob_name):
@@ -58,13 +59,13 @@ def write_blob(container_name, blob_name, data):
     blob_client.upload_blob(data, overwrite=True)
 
 # Reading hashtags.json from Blob Storage
-hashtags_blob = read_blob('tweeper-data', 'hashtags.json')
+hashtags_blob = read_blob('your-storage-container', 'hashtags.json')
 hashtags = json.loads(hashtags_blob)
 logging.info('Loaded hashtags from blob storage')
 
 # Reading and Writing tweet_history.json from Blob Storage
 try:
-    tweet_history_blob = read_blob('tweeper-data', 'tweet_history.json')
+    tweet_history_blob = read_blob('your-storage-container', 'tweet_history.json')
     tweet_history = json.loads(tweet_history_blob)
     logging.info('Loaded tweet history from blob storage')
 except Exception as e:
@@ -82,7 +83,7 @@ def get_top_posts(subreddit, flair, time_filter='day', limit=1):
 
 def tweet_posts(posts, tweet_history):
     # Load tweet history
-    logging.info('Starting to tweet posts')
+    logging.info('Preparing to tweet posts')
     for post in posts:
         title, url, selftext = post
         logging.info(f'Processing post: {title}')
@@ -95,13 +96,17 @@ def tweet_posts(posts, tweet_history):
 
         # Hashtag collection
         tweet_hashtags = [hashtags[key] for key in hashtags if key in title.lower()]
-        tweet_hashtags = tweet_hashtags[:3]  # Limit to max 3 hashtags
-        logging.info(f'Collected hashtags for post: {tweet_hashtags}')
+        tweet_hashtags = tweet_hashtags[:2]  # Limit to max 2 hashtags
 
         if tweet_hashtags:
             tweet += " " + " ".join(tweet_hashtags)
         logging.info(f'Collected hashtags for post: {tweet_hashtags}') 
         
+        # Check if title is within the character limit
+        if len(title) > 280 - 23:
+            logging.warning(f'Title exceeds character limit: {title}')
+            continue
+                
         if tweet not in tweet_history:
             # Post tweet
             logging.info(f"Attempting to post tweet: {tweet}")
@@ -110,14 +115,18 @@ def tweet_posts(posts, tweet_history):
                 logging.info(f'Successfully posted tweet: {tweet}')
                 # Add tweet to history after successful posting
                 tweet_history.append(tweet)
-            except tweepy.errors.Forbidden:
-                logging.error(f'Failed to post tweet: {tweet}')
-                continue
+            except tweepy.errors.TooManyRequests as e:
+                if 'Rate limit exceeded' in str(e.reason):
+                    logging.warning('Rate limit exceeded, skipping this tweet...')
+                    continue
+                else:
+                    logging.warning(f'Failed to post tweet: {tweet}')
+                    continue
 
     # Write updated tweet history to blob storage
     if tweet_history:
         updated_tweet_history = json.dumps(tweet_history)
-        write_blob('tweeper-data', 'tweet_history.json', updated_tweet_history)
+        write_blob('your-storage-container', 'tweet_history.json', updated_tweet_history)
         logging.info('Updated tweet history written to blob storage')
 
 def main(mytimer: func.TimerRequest = None) -> None:
@@ -129,7 +138,6 @@ def main(mytimer: func.TimerRequest = None) -> None:
         top_general = get_top_posts('cybersecurity', 'News - General')
 
         # Tweeting posts
-        logging.info('Tweeting posts')
         tweet_posts(top_breaches, tweet_history)
         tweet_posts(top_general, tweet_history)
 
